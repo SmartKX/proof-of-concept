746a6b0bd5fc44204c13903049426dd45c3adf1d
Cody Marx Bailey <superphly@gmail.com>
2018-12-04 11:33:02 -0600

    updates

--- js/main.js
+++ js/main.js
@@ -20,13 +20,11 @@ window.addEventListener('load', async () => {
 	}
 });
 
-var address  = '0x28d3cc9f8ba51d72e9d6e013eb0bcb65b5007fab';
+var address  = '0x28D3CC9F8bA51D72E9d6e013EB0bCB65b5007fab';
 var abi = [{"constant":true,"inputs":[],"name":"eMap","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"breaks","outputs":[{"name":"","type":"int64"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_year","type":"uint16"},{"name":"_quarter","type":"uint8"}],"name":"calculate","outputs":[],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"numAccounts","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_year","type":"uint16"},{"name":"_quarter","type":"uint8"},{"name":"_account","type":"uint8"}],"name":"getAccountValue","outputs":[{"name":"","type":"int64"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"keyOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getFeeSchedule","outputs":[{"name":"","type":"int64[]"},{"name":"","type":"int64[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_year","type":"uint16"},{"name":"_quarter","type":"uint8"},{"name":"_account","type":"uint8"},{"name":"_value","type":"int64"}],"name":"setAccountValue","outputs":[{"name":"","type":"int64"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"rates","outputs":[{"name":"","type":"int8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"keyManager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_year","type":"uint16"},{"name":"_quarter","type":"uint8"}],"name":"getAccountValues","outputs":[{"name":"","type":"int64[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint16"},{"name":"","type":"uint8"},{"name":"","type":"uint8"}],"name":"accounts","outputs":[{"name":"","type":"int64"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_keyManager","type":"address"},{"name":"_numAccounts","type":"uint8"},{"name":"_eMap","type":"string"},{"name":"_breaks","type":"int64[]"},{"name":"_rates","type":"int8[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"","type":"int256"}],"name":"ReportAum","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"","type":"int64[]"}],"name":"ReportSplits","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"","type":"uint256"}],"name":"ReportFeeTotal","type":"event"}];
 
 let contract = web3.eth.contract(abi);
 let skx = contract.at(address);
-
-let escan = "https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=0xd1Df4eFc6b7d47D00E21566B668a9cbbBf5D26D0&startblock=0&endblock=99999999&sort=asc&apikey=E2Z51ZDBPTNM2XXX4MXZIDUZHVWY5MQVY6";
 
 $('.skx-account-value').each(function(i,o) {
   skx.getAccountValue(2018,1,i, (e,v) => $(o).val(v))
@@ -51,3 +49,57 @@ var selector = new Vue({
     ]
   }
 });
+
+var rates = [
+  {lower: 0, upper: 1000000, rate: .01, amount: 0, result: 0},
+  {lower: 1000000.01, upper: 3000000, rate: .008, amount: 0, result: 0},
+  {lower: 3000000.01, upper: 5000000, rate: .006, amount: 0, result: 0},
+  {lower: 5000000.01, upper: Infinity, rate: .004, amount: 0, result: 0}
+];
+
+
+$('#calc').click(maths);
+
+function maths() {
+  let total = 0;
+  let totalBilled = 0;
+  // collect
+  $('input[type=text].skx-account-value').val(
+    function(a, b) {
+      total += parseInt(b.replace(/,/g, ''));
+      return b;
+    }
+  );
+
+  // gear on
+  $('#gear').show();
+
+  // compute
+  rates.forEach(function(rate) {
+    if (total > rate.lower) {
+      console.log(`In range! ${rate.lower} - ${rate.upper}`);
+      rate.amount = _.clamp(total, rate.lower, rate.upper);
+      rate.result = rate.amount * rate.rate;
+    }
+  });
+  
+  totalBilled = rates[0].result + rates[1].result + rates[2].result;
+
+  console.log(`Total - ${total.toLocaleString()}`);
+  console.log(`Total Billed ${totalBilled.toLocaleString()}`);
+
+  setTimeout(function() {
+    $('#total').text('$' + total.toLocaleString());
+    $('#totalBilled').text('$' + totalBilled.toLocaleString());
+    $('#rate0 td.amount').text('$' + rates[0].amount.toLocaleString());
+    $('#rate0 td.result').text('$' + rates[0].result.toLocaleString());
+    $('#rate1 td.amount').text('$' + rates[1].amount.toLocaleString());
+    $('#rate1 td.result').text('$' + rates[1].result.toLocaleString());
+    $('#rate2 td.amount').text('$' + rates[2].amount.toLocaleString());
+    $('#rate2 td.result').text('$' + rates[2].result.toLocaleString());
+
+    total = 0;
+    totalBilled = 0;
+    $('#gear').hide();
+  }, 5000);
+}
